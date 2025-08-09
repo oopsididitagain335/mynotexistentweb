b957ccb5fceff4ae68412e5ac78c8680139910fc
Solace <monkdev9@gmail.com>
2025-08-09 04:06:45 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,7 @@
-import '../styles/globals.css'; // adjust to your global CSS path
+// pages/_app.tsx
+import '../styles/globals.css'; // your global styles
 import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react'; // if you plan to use NextAuth
 
-export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
