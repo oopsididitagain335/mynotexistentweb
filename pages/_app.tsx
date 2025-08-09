8148547ea892ba3ef347a7b2995a10c8e7d1dc56
Solace <monkdev9@gmail.com>
2025-08-09 12:08:32 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,13 @@
 // pages/_app.tsx
-import React from 'react';
-import type { AppProps } from 'next/app';
-import { AuthProvider } from '@contexts/AuthContext';
-import { ThemeProvider } from '@contexts/ThemeContext';
 import { DiscoveryProvider } from '@contexts/DiscoveryContext';
-import { MessageProvider } from '@contexts/MessageContext';
-import { SecurityProvider } from '@contexts/SecurityContext';
-import Layout from '@components/Layout';
+import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <AuthProvider>
-        <SecurityProvider>
-          <DiscoveryProvider>
-            <MessageProvider>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </MessageProvider>
-          </DiscoveryProvider>
-        </SecurityProvider>
-      </AuthProvider>
-    </ThemeProvider>
+    <DiscoveryProvider>
+      <Component {...pageProps} />
+    </DiscoveryProvider>
   );
 }
 
